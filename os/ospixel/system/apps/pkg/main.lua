9ac7c49dec01b15d99686bee0aebaca9cb38fdd2
Kozodoychik <dimasikanovikov@gmail.com>
2022-02-17 15:48:01 +0300

    17.02.2022 Fixed bug in pkg

--- os/ospixel/system/apps/pkg/main.lua
+++ os/ospixel/system/apps/pkg/main.lua
@@ -1,7 +1,7 @@
 os.loadAPI("/ospixel/system/apis/registry.lua")
 local args = {...}
 if args[1] == "install" then
-    if not args[4] == "silent" then
+    if args[4] ~= "silent" then
     	print("Installing package: "..args[2])
     end
     local req = http.get("http://kotetube.7m.pl/ospixel/pkgs/"..args[2].."/filesToDownload")
@@ -10,7 +10,7 @@ if args[1] == "install" then
     	local text = http.get("http://kotetube.7m.pl/ospixel/pkgs/"..args[2].."/"..value)
         if args[3] == nil then
             fs.makeDir('/ospixel/system/apps/pkg')
-    	    local file = fs.open("/ospixel/users/"..registry.readKey("user","username").."/apps/"..args[2].."/"..value,'w')
+    	    local file = fs.open("/ospixel/user/"..registry.readKey("user","username").."/apps/"..args[2].."/"..value,'w')
     	    file.write(text.readAll())
             text.close()
     	    file.close()
@@ -21,11 +21,11 @@ if args[1] == "install" then
     	    file.close()
         end
     end
-    if not args[4] == "silent" then
+    if args[4] ~= "silent" then
     	print("Done!")
     end
 elseif args[1] == "uninstall" then
-    if not args[3] == "silent" then
+    if args[3] ~= "silent" then
     	print("Deleting package: "..args[2])
     end
     fs.delete("/ospixel/user/"..registry.readKey("user","username").."/apps/"..args[3])
