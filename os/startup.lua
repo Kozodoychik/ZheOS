2c81084e5a45cd4a5620b3f59e462f2809a84f65
Kozodoychik <dimasikanovikov@gmail.com>
2022-02-17 13:31:29 +0300

    Startup file

--- os/startup.lua
+++ os/startup.lua
@@ -1,7 +1,66 @@
+local fsOpen = _G['fs']['open']
+local fsDelete = _G['fs']['delete']
+local fsMove = _G['fs']['move']
+local fsCopy = _G['fs']['copy']
 term.clear()
 paintutils.drawImage(paintutils.loadImage("/ospixel/logo.nfp"),23,5)
 term.setBackgroundColor(colors.black)
 term.setCursorPos(23,13)
 print("OSPixel")
-os.sleep(4)
-shell.run("/ospixel/system/init.lua")
+_G['fs']['open'] = function(path,dest)
+   if shell.resolveProgram(path) == shell.resolveProgram("/startup.lua") then
+        error("Access denied",0)
+        return nil
+   else
+        return fsOpen(path,dest)
+   end
+end
+_G['fs']['move'] = function(path,mode)
+   if shell.resolveProgram(path) == shell.resolveProgram("/startup.lua") then
+        error("Access denied",0)
+        return nil
+   else
+        return fsMove(path,mode)
+   end
+end
+_G['fs']['copy'] = function(path,mode)
+   if shell.resolveProgram(path) == shell.resolveProgram("/startup.lua") then
+        error("Access denied",0)
+        return nil
+   else
+        return fsCopy(path,mode)
+   end
+end
+_G['fs']['delete'] = function(path)
+   if shell.resolveProgram(path) == shell.resolveProgram("/startup.lua") then
+        error("Access denied",0)
+        return nil
+   else
+        return fsDelete(path)
+   end
+end
+local inRecovery = false
+local i = 0
+function wait()
+    while i<100 or inRecovery do
+        i = i + 1
+        os.sleep(0.05)
+    end
+end
+function waitForKey()
+    while true do
+        local event, pressed = os.pullEvent("key")
+        if pressed == keys.leftShift then
+            inRecovery = true
+            shell.run("/ospixel/recovery/main.lua")
+        end
+    end
+end
+function init()
+    parallel.waitForAny(wait,waitForKey)
+    shell.run("/ospixel/system/init.lua")
+end
+local ok = pcall(init)
+if not ok then
+   os.reboot() 
+end
