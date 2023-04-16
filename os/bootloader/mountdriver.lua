ae6dc3ef34f374be2db9cb26e118028637d01ca5
Kozodoychik <dimasikanovikov@gmail.com>
2023-04-16 14:20:13 +0300

    Драйвер монтирования каталогов

--- os/bootloader/mountdriver.lua
+++ os/bootloader/mountdriver.lua
@@ -20,3 +20,29 @@ fs.list = function(path)
     end
     return fsList(point.."/"..path)
 end
+fs.isDir = function(path)
+    local point = nil
+    for k,v in pairs(mountPoints) do
+        if path.sub(path, 1, string.len(k)) then
+            point = v
+            break
+        end
+    end
+    if not point then
+        return fsIsDir(path)
+    end
+    return fsIsDir(point.."/"..path)
+end
+fs.exists = function(path)
+    local point = nil
+    for k,v in pairs(mountPoints) do
+        if path.sub(path, 1, string.len(k)) then
+            point = v
+            break
+        end
+    end
+    if not point then
+        return fsExists(path)
+    end
+    return fsExists(point.."/"..path)
+end
