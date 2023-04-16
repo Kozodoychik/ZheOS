e6ecd2313a4176d61fbee3c0794f1dade11032ab
Kozodoychik <dimasikanovikov@gmail.com>
2023-04-16 11:58:38 +0300

    Инструмент для создания образа из папки

--- os/makeimg.lua
+++ os/makeimg.lua
@@ -1,6 +1,18 @@
 local args = {...}
 local files = fs.list(args[1])
 local img = {}
-for v in pairs(files) do
-    img[v] = {}
+local loadFolder = function(img, path)
+    for k,v in ipairs(files) do
+        img[k].isDir = false
+        img[k].content = nil
+        if fs.isDir(path.."/"..k) then
+            img[k].isDir = true
+            img[k].content = loadFolder(img[k].content, path.."/"..k)
+            
+        end
+    end
+    return img
 end
+img = loadFolder(img, shell.resolve(args[1]))
+local outputFile = fs.open("image.img","w")
+outputFile.write(textutils.serialize(img))
