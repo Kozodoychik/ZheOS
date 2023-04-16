0114bb3af99ae5617141aa218c13098dded99432
Kozodoychik <dimasikanovikov@gmail.com>
2023-04-16 21:20:58 +0300

    Поправка для корректной работы

--- os/zheos/system/apps/pkg/main.lua
+++ os/zheos/system/apps/pkg/main.lua
@@ -1,4 +1,4 @@
-os.loadAPI(_SYSPATH.."system/apis/registry.lua")
+os.loadAPI("apis/registry.lua")
 local args = {...}
 if args[1] == "install" then
     if args[4] ~= "silent" then
@@ -12,8 +12,8 @@ if args[1] == "install" then
 	for key, value in ipairs(files) do
     	local text = http.get("http://kotetube.7m.pl/zheos/pkgs/"..args[2].."/"..value)
         if args[3] == nil then
-            fs.makeDir(_SYSPATH..'system/apps/pkg')
-    	    local file = fs.open(_SYSPATH.."user/"..registry.readKey("user","username").."/apps/"..args[2].."/"..value,'w')
+            fs.makeDir('apps/pkg')
+    	    local file = fs.open("user/"..registry.readKey("user","username").."/apps/"..args[2].."/"..value,'w')
     	    file.write(text.readAll())
             text.close()
     	    file.close()
@@ -31,5 +31,5 @@ elseif args[1] == "uninstall" then
     if args[3] ~= "silent" then
     	print("Deleting package: "..args[2])
     end
-    fs.delete(_SYSPATH.."user/"..registry.readKey("user","username").."/apps/"..args[3])
+    fs.delete("user/"..registry.readKey("user","username").."/apps/"..args[3])
 end
