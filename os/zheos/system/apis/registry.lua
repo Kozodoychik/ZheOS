f31f2af5378a7a0331bad1bada7eafc626ae8cc6
Kozodoychik <dimasikanovikov@gmail.com>
2023-05-17 17:55:55 +0300

    Новый принцип работы реестра

--- os/zheos/system/apis/registry.lua
+++ os/zheos/system/apis/registry.lua
@@ -1,6 +1,6 @@
 local regpath = '.registry'
 local function getKeyFromPath(path)
-    local node = _G['registry']
+    local node = _G['reg']
     for k,v in ipairs(path) do
         node = node[v]
         if node == nil then
@@ -21,21 +21,21 @@ local function createKeyFromPath(value, path)
       createKeyFromPath(table2[v], value, path)
     end
     for k,v in pairs(table2) do
-        _G['registry'][k] = v
+        _G['reg'][k] = v
     end
 end
 function saveRegistry()
     file = fs.open(regpath, "w")
-    file.write(textutils.serialise(_G['registry']))
+    file.write(textutils.serialise(_G['reg']))
     file.close()
 end
 function loadRegistry()
     file = fs.open(regpath, "r")
-    _G['registry'] = textutils.unserialise(file.readAll())
+    _G['reg'] = textutils.unserialise(file.readAll())
     file.close()
 end
 function createRegistry()
-    _G['registry'] = {}
+    _G['reg'] = {}
     saveRegistry()
 end
 function createKey(value, ...)
