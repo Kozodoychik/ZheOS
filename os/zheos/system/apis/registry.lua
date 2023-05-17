a510e64b0fc4f9ca045e1a05cef35de6e0da47ce
Kozodoychik <dimasikanovikov@gmail.com>
2023-05-17 15:11:15 +0300

    Новый принцип работы реестра

--- os/zheos/system/apis/registry.lua
+++ os/zheos/system/apis/registry.lua
@@ -1,15 +1,40 @@
-regpath = '.registry'
-function createReg()
-    fs.makeDir(regpath)
-    fs.makeDir(regpath..'/system')
-    fs.makeDir(regpath..'/user')
+local regpath = '.registry'
+local registry = {}
+local function saveRegistry()
+    file = fs.open(regpath, "w")
+    file.write(textutils.serialise(registry))
+    file.close()
 end
-function createKey(regname, keyname, keycontent)
-    reg = fs.open(regpath..'/'..regname..'/'..keyname, 'w')
-    reg.writeLine(keycontent)
-    reg.close()
+local function getKeyFromPath(table, path)
+    local node = table
+    for v in pairs(path) do
+        node = node[v]
+        if node == nil then
+            error("invalid table path")
+            return
+        end
+    end
+    return node
 end
-function readKey(regname, keyname)
+local function createKeyFromPath(table, path)
+    local node = table
+    for v in pairs(path) do
+        node = node[v]
+    end
+end
+function loadRegistry()
+    file = fs.open(regpath, "r")
+    registry = textutils.unserialise(file.readAll())
+    file.close()
+end
+function createRegistry()
+    saveRegistry()
+end
+function createKey(...)
+    local path = table.pack(...)
+    local key = createKeyFromPath(registry, path)
+end
+function readKey(...)
     reg = fs.open(regpath..'/'..regname..'/'..keyname, 'r')
     local content = reg.readLine()
     reg.close()
