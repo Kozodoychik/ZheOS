c75000c361cd785bfdbab47fa00a7fff337b3a72
Kozodoychik <dimasikanovikov@gmail.com>
2023-05-20 10:43:58 +0300

    Новый установщик

--- install.lua
+++ install.lua
@@ -9,14 +9,24 @@ local filesToDownload = {
 	"zheos/system/utils/cat.lua",
 	"zheos/recovery/init.lua"
 }
-function download(filename)
+
+local function preLoad()
+	print("Downloading GUI API...")
+	local req = http.get("https://raw.githubusercontent.com/Kozodoychik/ZheOS/1.0.0/os/zheos/system/apis/gui.lua")
+	local file = fs.open("/.temp/gui.lua","w")
+	file.write(req.readAll())
+	file.close()
+	os.loadAPI("/.temp/gui.lua")
+end
+
+local function download(filename)
 	local req = http.get("https://raw.githubusercontent.com/Kozodoychik/ZheOS/1.0.0/os/"..filename)
 	local file = fs.open("/"..filename,"w")
 	file.write(req.readAll())
 	file.close()
 end
 
-function install()
+local function install()
 	gui.destroyButton("installBtn")
 	gui.newProgressBar("progress", 2, 10, 49, colors.lime, 0)
 	gui.newLabel("file", 2, 9, "", colors.lightGray)
@@ -28,27 +38,29 @@ function install()
 	end
 	gui.exit()
 end
-print("Downloading GUI API...")
-local req = http.get("https://raw.githubusercontent.com/Kozodoychik/ZheOS/1.0.0/os/zheos/system/apis/gui.lua")
-local file = fs.open("/.temp/gui.lua","w")
-file.write(req.readAll())
-file.close()
-os.loadAPI("/.temp/gui.lua")
+
+local function makeDirs()
+	fs.delete("/.temp")
+	fs.makeDir("/zheos/user")
+	fs.makeDir("/zheos/system/user")
+	fs.makeDir("/zheos/system/rom")
+	fs.makeDir("/zheos/recovery/rom")
+end
+
+local function createConfig()
+	local cfg = {default=1,loadPaths={"/zheos/system/","/zheos/recovery/","/"},labels={"ZheOS Init","Recovery","CraftOS"}}
+	local cfgFile = fs.open("/zhestartup.cfg","w")
+	cfgFile.write(textutils.serialize(cfg))
+	cfgFile.close()
+end
+
+preLoad()
 gui.init()
-gui.setBGColor(colors.white)
-gui.newLabel("mainLabel", 1, 1, "ZheOS 1.0.0 Setup", colors.lightGray)
-gui.newButton("installBtn", 21, 8, "Install", colors.lime, install)
+gui.setBGColor(colors.lightGray)
+gui.newLabel("mainLabel", 1, 1, "ZheOS 1.0.0 Setup", colors.gray)
+gui.newButton("installBtn", 21, 8, "Install", colors.lightBlue, install)
+gui.newButton("exitBtn", 23, 8, "Exit", colors.lightBlue, os.reboot)
 gui.mainLoop()
-
-fs.delete("/.temp")
-fs.makeDir("/zheos/user")
-fs.makeDir("/zheos/system/user")
-fs.makeDir("/zheos/system/rom")
-fs.makeDir("/zheos/recovery/rom")
-
-local cfg = {default=1,loadPaths={"/zheos/system/","/zheos/recovery/","/"},labels={"ZheOS Init","Recovery","CraftOS"}}
-local cfgFile = fs.open("/zhestartup.cfg","w")
-cfgFile.write(textutils.serialize(cfg))
-cfgFile.close()
-
+makeDirs()
+createConfig()
 os.reboot()
